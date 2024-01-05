5fb3e8f99cb77090ad2947653f1190880d86d8af
Bùi Tống Minh Châu <buitongminhchau01032002@gmail.com>
2024-01-05 14:10:25 +0700

    update

--- src/layouts/components/Sidebar/index.jsx
+++ src/layouts/components/Sidebar/index.jsx
@@ -33,7 +33,7 @@ const groupMenus = [
     },
     {
         main: {
-            iconClassname: 'fa-solid fa-box-open',
+            iconClassname: 'fa-solid fa-file-import',
             text: 'Phiếu nhập',
             link: '/import',
         },
@@ -98,7 +98,7 @@ const groupMenus = [
     },
     {
         main: {
-            iconClassname: 'fa-solid fa-clipboard',
+            iconClassname: 'fa-solid fa-tag',
             text: 'Phiếu giảm giá',
             link: '/coupon',
         },
@@ -140,19 +140,19 @@ const groupMenus = [
     },
     {
         main: {
-            iconClassname: 'fa-solid fa-clipboard',
+            iconClassname: 'fa-solid fa-chart-simple',
             text: 'Thống kê',
             link: '/statistic',
         },
         children: [
             {
-                iconClassname: 'fa-solid fa-list',
+                iconClassname: 'fa-solid fa-box-open',
                 text: 'Sản phẩm',
                 link: '/product',
                 functionName: 'statistic/product',
             },
             {
-                iconClassname: 'fa-solid fa-list',
+                iconClassname: 'fa-solid fa-money-bill-trend-up',
                 text: 'Doanh số',
                 link: '/profit',
                 functionName: 'statistic/profit',
@@ -182,7 +182,7 @@ const groupMenus = [
     },
     {
         main: {
-            iconClassname: 'fa-solid fa-clipboard',
+            iconClassname: 'fa-solid fa-user-tag',
             text: 'Chức vụ',
             link: '/role',
         },
@@ -208,7 +208,7 @@ function Sidebar() {
         <div className="h-full min-w-[240px] bg-blue-500">
             <header className="flex h-20 w-full flex-col items-center justify-center border-b border-white/40 text-white">
                 <div className="text-lg font-extrabold">QUẢN LÝ</div>
-                <div className="font-bold">CỬA HÀNG CÂY XANH</div>
+                <div className="font-bold">CỬA HÀNG BÁN GIÀY</div>
             </header>
 
             <div className="">
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,39 +1,32 @@
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { accountActions } from '../../redux/slices/accountSlide';
+import { accountSelector } from '../../redux/selectors';
 
 function Home() {
     const dispatch = useDispatch();
+    const account = useSelector(accountSelector);
     const showLogoutNoti = () => toast.info('Đã đăng xuất!');
 
     return (
-        <div className="container flex h-full w-full items-center justify-center space-x-11">
-            <div className=" space-y-3 text-gray-600">
-                <p className="text-center text-4xl font-extrabold text-green-600">QUẢN LÝ</p>
-                <p className="text-center text-3xl font-bold text-green-600">CỬA HÀNG CÂY XANH</p>
-                <p className="text-xl font-bold">Nếu cần hỗ trợ kỹ thuật, vui lòng thực hiện một trong ba cách sau: </p>
-                <p className="text-lg ">
-                    1. Truy cập{' '}
-                    <a href="https://forum.uit.edu.vn/" className="underline hover:text-blue-600">
-                        https://forum.uit.edu.vn/
-                    </a>{' '}
-                    và gửi yêu cầu hỗ trợ.
+        <div className="container flex h-full w-full items-center justify-center space-x-11 px-10">
+            <div className="flex-1">
+                <img className="w-full object-cover" src="/banner.png" />
+            </div>
+
+            <div className="flex-1 text-gray-600">
+                <p className="mb-2 text-5xl font-extrabold text-blue-800">QUẢN LÝ</p>
+                <p className="mb-8 text-4xl font-bold text-blue-800">CỬA HÀNG BÁN GIÀY</p>
+
+                <p className="font-gray-900 mb-8 text-4xl font-bold">
+                    {'Xin chào, ' + account.name}
                 </p>
-                <p className="text-lg">
-                    2. Gửi email cho phòng kỹ thuật: <span className="underline">20521154@gm.uit.edu.vn</span>
-                    {'.'}
-                </p>
-                <p className="text-lg ">
-                    3. Gọi HOTLINE hỗ trợ khách hàng: <span className="underline">0365011369</span>
-                    {', '}
-                    <span className="underline">0523011333</span>
-                    {'.'}
-                </p>
+
                 <button
-                    className="btn btn-md btn-blue"
+                    className="btn btn-md btn-blue mb-10"
                     onClick={() => {
                         dispatch(accountActions.logout());
                         showLogoutNoti();
@@ -44,8 +37,28 @@ function Home() {
                     </span>
                     <span>Đăng xuất</span>
                 </button>
+
+                <p className="text-xl font-bold">
+                    Nếu cần hỗ trợ kỹ thuật, vui lòng thực hiện một trong ba cách sau:
+                </p>
+                <p className="text-lg ">
+                    <span>1. Truy cập </span>
+                    <a href="https://forum.uit.edu.vn/" className="underline hover:text-blue-600">
+                        https://forum.uit.edu.vn/
+                    </a>
+                    <span> và gửi yêu cầu hỗ trợ.</span>
+                </p>
+                <p className="text-lg">
+                    <span>2. Gửi email cho phòng kỹ thuật: </span>
+                    <span className="underline">20521154@gm.uit.edu.vn</span>
+                </p>
+                <p className="text-lg ">
+                    <span>
+                        <span>3. Gọi HOTLINE hỗ trợ khách hàng: </span>
+                        <span className="underline">0365011369</span>
+                    </span>
+                </p>
             </div>
-            <img className="w-[40vw] " src="/home-img.jpg" />
         </div>
     );
 }
